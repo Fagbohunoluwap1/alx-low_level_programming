6bf24856f3ea778f3638ac8cd646622b0fc10537
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-06-03 16:32:10 -0700

    C

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-        int i;
- 	int j;
- 	int k;
-
- 	for (i = 48; i < 56; i++)
- 	{
- 		for (j = i + 1; j < 57; j++)
+	int i;
+	int j;
+	int k;
+	
+	for (i = 48; i < 56; i++)
+	{
+		for (j = i + 1; j < 57; j++)
  		{
  			for (k = j + 1; k < 58; k++)
  			{
