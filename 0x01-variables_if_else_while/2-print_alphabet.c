cb86e9374c5dc481e3468576639c2a46fa4183de
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-06-03 08:34:15 -0700

    C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print lower case a-z
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+	
+	while (alpha <='z');
+	{
+		putchar(alpha);
+		alpha++;
+	}      
+	putchar ('\n');
+
+	return (0);
+}
