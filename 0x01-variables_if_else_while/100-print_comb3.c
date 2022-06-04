55aff65622bb9bd7b0acb6ef3461229fbda766cf
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-06-03 17:42:20 -0700

    programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - a simple program that outputs 0-9 separated by commas
- *
- * Return: 0 on success
+ * main - prints all possible different combinations of two digits
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int i;
-	int j;
-	int k;
+	int i, j;
 
-	for (i = 48; i < 56; i++)
+	for (i = 48; i <= 56; i++)
 	{
-		for (j = i + 1; j < 57; j++)
+		for (j = 49; j <= 57; j++)
 		{
-			for (k = j + 1; k < 58; k++)
+			if (j > i)
 			{
 				putchar(i);
 				putchar(j);
-				putchar(k);
-				if (i != 55 || j != 56 || k != 57)
+				if (i != 56 || j != 57)
 				{
-					putchar(',');				}
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
 	putchar('\n');
 	return (0);
-	}
+}
