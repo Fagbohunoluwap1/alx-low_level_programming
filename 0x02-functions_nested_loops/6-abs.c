16c5b1574cb46ac5edb00f4649e90cd1030d5122
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-06-07 07:48:00 -0700

    putchar

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * -abs - computes the absolute value of an integer
+ * _abs - computes the absolute value of an integer
  *  @n: the int to check
  *  Return: the absolute value of int
  */
-int _abs(int)
+int _abs(int n)
 {
 	if (n >= 0)
 	{
