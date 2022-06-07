4758defac5e9e283e37ccac93b4044d9955cf360
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-06-07 06:44:36 -0700

    putchar

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,10 +2,10 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @c: the character to check
+ * @c: the characters to checked
  * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
