170e8bb4d47f2dff5c40e9c0e196213c1348bf99
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-06-07 08:25:55 -0700

    putchar

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minutes of the day of Jack Bauer,
+ * jack_bauer - prints every minutes of the day of Jack Bauer
  * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
@@ -12,8 +12,8 @@ void jack_bauer(void)
 
 	while (i < 24)
 	{
-		j = 0
-			while (j < 60)
+		j = 0;
+		while (j < 60)
 			{
 				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
