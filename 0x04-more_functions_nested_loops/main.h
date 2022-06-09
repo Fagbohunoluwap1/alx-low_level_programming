d9d38d335310296aa41c9483e20e847011023039
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-06-09 07:22:06 -0700

    nestedloop

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,12 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int C);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void print_line(int n);
+
+#endif
