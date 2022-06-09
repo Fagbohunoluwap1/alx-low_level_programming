8117ff79bded1dcc0b8c7dfb51fec1cac27ad6f1
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-06-09 05:58:34 -0700

    nestedloop

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_most_numbers - prints the numbers, from 0 to 9,
- * exept 2 and 4, followed by a new line
+ * except 2 and 4, followed by a new line
  */
 void print_most_numbers(void)
 {
@@ -12,7 +12,7 @@ void print_most_numbers(void)
 	{
 		if (i != 2 && i != 4)
 		{
-			_putchar(i);
+			_putchar(i + '0');
 		}
 	}
 
