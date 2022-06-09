48b75b35469b40591cfdeeb7fe37490a2937bf68
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-06-09 10:08:49 -0700

    coding

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,15 +16,15 @@ void print_triangle(int size)
 		for (i = 1; i <= size; i++)
 		{
 			for (j = i; j < size; j++)
-		{
-			_putchar(' ');
-		}
+			{
+				_putchar(' ');
+			}
 
 			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
-			
+
 			_putchar('\n');
 		}
 	}
