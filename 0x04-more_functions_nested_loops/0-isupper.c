81c8d4041cd49e891573c37ac5d57892f85acbef
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-06-09 04:13:23 -0700

    nestedloops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if c is upper case, 0 otherwise
+ */
+int -isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
