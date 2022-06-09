94d1fc0721a3f55212f4ee18ae9bd6931cf1cf19
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-06-09 07:58:59 -0700

    morenestedloop

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,7 +13,6 @@ void print_diagonal(int n)
 	{
 		int i, j;
 
-
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
@@ -21,7 +20,8 @@ void print_diagonal(int n)
 				if (j == i)
 					_putchar('\\');
 				else if (j < i)
-					_putchar(' ');}
+					_putchar(' ');
+			}
 			_putchar('\n');
 		}
 	}
