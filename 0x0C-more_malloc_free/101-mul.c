3628a67c3c8c8feb6ce66637ea0c1231f932967e
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-06-30 05:02:51 -0700

    moremalloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,7 +6,7 @@
  * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
  *
- * Return: 0 if non-digit is found, 1 otherwise
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *s)
 {
@@ -52,7 +52,7 @@ void errors(void)
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: always 0 (success)
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
