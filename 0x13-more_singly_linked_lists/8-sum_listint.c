dc4238ef095ff02d08aff4016a4635de9d027917
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-07-19 03:22:38 -0700

    moresinglylinkedlist

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - program returns the sum of all data (n)
+ * of a linked list
+ * @head: head of a list
+ *
+ * Return:sum of all data (n)
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
